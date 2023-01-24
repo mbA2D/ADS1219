ade07f789dbc6207357dda812ae18f5a3ce856da
mbA2D <ottawaelectronics2016@gmail.com>
2023-01-24 00:15:36 -0500

    Compiling properly in Arduino IDE

---
+++ ADS1219.cpp
@@ -0,0 +1,215 @@
+#include <ADS1219.h>
+#include "Arduino.h"
+
+ADS1219::ADS1219()
+{
+	_set_reg_defaults();
+	_offset = 0;
+	_ext_ref_v = 0;
+}
+
+void ADS1219::init(uint8_t address, TwoWire *i2c)
+{
+	_addr = address;
+	_i2c = i2c;
+	
+	//check if i2c->begin has already been called.
+	if (TWCR == 0)
+	{
+		_i2c->begin();
+	}
+	
+	_set_reg_defaults();
+}
+
+void ADS1219::reset()
+{
+	_write_command_byte(ADS1219_COMMAND_RESET);
+	_set_reg_defaults();
+}
+
+void ADS1219::set_conf_reg(uint8_t conf_byte)
+{
+	_conf_reg.conf_byte = conf_byte;
+	_write_conf_reg();
+}
+
+void ADS1219::set_mux(uint8_t mux)
+{
+	if (mux <= ADS1219_MUX_P_AVDD_2_N_AVDD_2)
+	{
+		_conf_reg.bits.mux = mux;
+		_write_conf_reg();
+	}
+}
+
+void ADS1219::set_gain(uint8_t gain)
+{
+	if (gain <= ADS1219_GAIN_4)
+	{
+		_conf_reg.bits.gain = gain;
+		_write_conf_reg();
+	}
+}
+
+void ADS1219::set_dr(uint8_t dr)
+{
+	if (dr <= ADS1219_DR_1000SPS)
+	{
+		_conf_reg.bits.dr = dr;
+		_write_conf_reg();
+	}
+}
+
+void ADS1219::set_cm(uint8_t cm)
+{
+	if (cm <= ADS1219_CM_CONTINUOUS)
+	{		
+		_conf_reg.bits.cm = cm;
+		_write_conf_reg();
+	}
+}
+
+void ADS1219::set_vref(uint8_t vref)
+{
+	if (vref <= ADS1219_VREF_EXTERNAL)
+	{
+		_conf_reg.bits.vref = vref;
+		_write_conf_reg();
+	}
+}
+
+void ADS1219::calibrate_offset()
+{
+	//set ain to mid-supply (don't change other settings)
+	set_mux(ADS1219_MUX_P_AVDD_2_N_AVDD_2);
+	
+	int32_t min = 0;
+	int32_t max = 0;
+	int32_t sum = 0;
+	//get num_measurements values
+	for (uint8_t i = 0; i < ADS1219_NUM_MEASUREMENTS_FOR_OFFSET_CAL; i++)
+	{
+		_calibration_readings[i] = 0;
+		
+		int32_t reading = trigger_and_get_single_shot_result();
+		
+		//drop high and low as we read the values;
+		//average the rest
+		//everything should be close to 0 so taking the sum into a 32-bit int should be fine (no overflow).
+		if (reading > max)
+		{
+			_calibration_readings[i] = max;
+			max = reading;
+		}
+		else if (reading < min)
+		{
+			_calibration_readings[i] = min;
+			min = reading;
+		}
+		else
+		{
+			_calibration_readings[i] = reading;
+		}
+		sum += _calibration_readings[i];
+	}
+	
+	_offset = sum / (ADS1219_NUM_MEASUREMENTS_FOR_OFFSET_CAL - 2);
+}
+
+void ADS1219::trigger_conversion()
+{
+	_write_command_byte(ADS1219_COMMAND_START_SYNC);
+}
+
+void ADS1219::shut_down()
+{
+	_write_command_byte(ADS1219_COMMAND_POWERDOWN);
+}
+
+int32_t ADS1219::trigger_and_get_single_shot_result()
+{
+	//assuming already in single-shot mode (would also work in continuous mode
+	trigger_conversion();
+	while (!get_drdy())
+	{
+		delayMicroseconds(1100); //max sample rate is 1000SPS. add a little headroom for the delay.
+	}
+	return get_conversion_result();
+}
+
+int32_t ADS1219::get_conversion_result()
+{
+	uint8_t bit1, bit2, bit3;
+	int32_t result = 0;
+	
+	_write_command_byte(ADS1219_COMMAND_RDATA);
+	
+	uint8_t response_length = 3;
+	_i2c->requestFrom(_addr, response_length);
+	bit1 = _i2c->read();
+	bit2 = _i2c->read();
+	bit3 = _i2c->read();
+	
+	result = (bit1<<24) & (bit2<<16) & (bit3<<8); //bitshift by 8 extra to preserve 2's complement math.
+	result = result / 256;
+	result = result - _offset;
+	return result;
+}
+
+bool ADS1219::get_drdy()
+{
+	_write_command_byte(ADS1219_COMMAND_RREG_STATUS);
+	
+	uint8_t response_length = 1;
+	_i2c->requestFrom(_addr, response_length);
+	_status_reg.status_byte = _i2c->read();
+	return bool(_status_reg.bits.drdy);
+}
+
+void ADS1219::set_ext_ref_v_for_calc(float ref_v)
+{
+	_ext_ref_v = ref_v;
+}
+
+float ADS1219::measure_voltage()
+{
+	//this function should return the voltage at the pins of the ADC.
+	
+	//vref
+	float ref_v;
+	if(_conf_reg.bits.vref == ADS1219_VREF_INTERNAL) ref_v = ADS1219_VOLTAGE_REFERENCE_V;
+	else ref_v = _ext_ref_v;
+
+	//gain
+	uint8_t gain;
+	if (_conf_reg.bits.gain == ADS1219_GAIN_1) gain = 1;
+	else gain = 4;
+	
+	int32_t reading = trigger_and_get_single_shot_result();
+	
+	return ((reading / ADS1219_FULL_SCALE_POS_CODE) * (ref_v / gain));
+
+}
+
+void ADS1219::_set_reg_defaults()
+{
+	//set everything back to 0 (default)
+	_conf_reg.conf_byte = 0;
+	_status_reg.status_byte = 0;
+}
+
+void ADS1219::_write_command_byte(uint8_t command_byte)
+{
+	_i2c->beginTransmission(_addr);
+	_i2c->write(command_byte);
+	_i2c->endTransmission();
+}
+
+void ADS1219::_write_conf_reg()
+{
+	_i2c->beginTransmission(_addr);
+	_i2c->write(ADS1219_COMMAND_WREG);
+	_i2c->write(_conf_reg.conf_byte);
+	_i2c->endTransmission();
+}
---
+++ ADS1219.h
@@ -0,0 +1,114 @@
+#ifndef ADS1219_h
+#define ADS1219_h
+
+#include "Wire.h"
+
+#define ADS1219_VOLTAGE_REFERENCE_V					2.048
+#define ADS1219_FULL_SCALE_POS_CODE					0x7FFFFF //2^23
+#define ADS1219_FULL_SCALE_NEG_CODE					0x800000 //-(2^23)
+#define ADS1219_NUM_MEASUREMENTS_FOR_OFFSET_CAL		5
+
+//############# COMMAND BYTES ###############
+#define ADS1219_COMMAND_RESET		0x06
+#define ADS1219_COMMAND_START_SYNC	0x08
+#define ADS1219_COMMAND_POWERDOWN	0x02
+#define ADS1219_COMMAND_RDATA		0x10
+#define ADS1219_COMMAND_RREG_CONF	0x20 //read command register
+#define ADS1219_COMMAND_RREG_STATUS	0x24 //read status register
+#define ADS1219_COMMAND_WREG		0x40 //writes to configuration register
+
+//############# REGISTER VALUES ##############
+#define ADS1219_MUX_P_AIN0_N_AIN1		0
+#define ADS1219_MUX_P_AIN2_N_AIN3		1
+#define ADS1219_MUX_P_AIN1_N_AIN2		2
+#define ADS1219_MUX_P_AIN0_N_AGND		3
+#define ADS1219_MUX_P_AIN1_N_AGND		4
+#define ADS1219_MUX_P_AIN2_N_AGND		5
+#define ADS1219_MUX_P_AIN3_N_AGND		6
+#define ADS1219_MUX_P_AVDD_2_N_AVDD_2	7
+
+#define ADS1219_GAIN_1			0
+#define ADS1219_GAIN_4			1
+
+#define ADS1219_DR_20SPS		0
+#define ADS1219_DR_90SPS		1
+#define ADS1219_DR_330SPS		2
+#define ADS1219_DR_1000SPS		3
+
+#define ADS1219_CM_SINGLE_SHOT	0
+#define ADS1219_CM_CONTINUOUS	1
+
+#define ADS1219_VREF_INTERNAL 	0
+#define ADS1219_VREF_EXTERNAL	1
+
+#define ADS_1219_DRDY_FALSE		0
+#define ADS_1219_DRDY_TRUE		1
+
+typedef union _ads1219_conf_reg
+{
+	struct
+	{
+		uint8_t mux: 3;
+		uint8_t gain: 1;
+		uint8_t dr: 2;
+		uint8_t cm: 1;
+		uint8_t vref: 1;
+	}bits;
+	uint8_t conf_byte;
+}ads1219_conf_reg;
+
+typedef union _ads1219_status_reg
+{
+	struct
+	{
+		uint8_t drdy: 1;
+		uint8_t res: 7; //reserved
+	}bits;
+	uint8_t status_byte;
+}ads1219_status_reg;
+
+
+class ADS1219
+{
+	public:
+		ADS1219();
+	
+		void init(uint8_t address, TwoWire *i2c = &Wire);
+		void reset();
+		
+		void set_conf_reg(uint8_t conf_byte);
+		void set_mux(uint8_t mux);
+		void set_gain(uint8_t gain);
+		void set_dr(uint8_t dr);
+		void set_cm(uint8_t cm);
+		void set_vref(uint8_t vref);
+		
+		void calibrate_offset();
+		void trigger_conversion(); //also wakes up the analog portions of the device
+		void shut_down(); //shuts down analog portions of the device, stops continuous conversions
+		
+		int32_t trigger_and_get_single_shot_result();
+		int32_t get_conversion_result();
+		bool get_drdy();
+		
+		void set_ext_ref_v_for_calc(float ref_v);
+		float measure_voltage(); //uses single-shot mode
+	
+	private:
+		void _set_reg_defaults();
+		void _write_command_byte(uint8_t command_byte);
+		void _write_conf_reg();
+		
+		int32_t _calibration_readings[ADS1219_NUM_MEASUREMENTS_FOR_OFFSET_CAL];
+		int32_t _offset;
+		uint8_t _addr;
+		TwoWire *_i2c;
+		
+		float _ext_ref_v;
+		
+		ads1219_conf_reg	_conf_reg;
+		ads1219_status_reg	_status_reg;
+	
+};
+
+#endif
